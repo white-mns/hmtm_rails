1a4d2bf1e226f0ddd0f266c42c1805a2d9d81f83
shiro <shiro.mns@gmail.com>
2022-02-07 16:02:44 +0900

    [追加]index以外をルーティング時点で受け付けなくする処理

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  resources :obsolescences
-  resources :spells
-  resources :tune_gem_data
-  resources :spell_data
-  resources :items
+  resources :obsolescences, only: [:index]
+  resources :spells, only: [:index]
+  resources :tune_gem_data, only: [:index]
+  resources :spell_data, only: [:index]
+  resources :items, only: [:index]
   get "equip/graphs", :to => "equips#graphs", :as => "equip_graphs"
-  resources :equips
-  resources :statuses
+  resources :equips, only: [:index]
+  resources :statuses, only: [:index]
   get "profile/graphs", :to => "profiles#graphs", :as => "profile_graphs"
-  resources :profiles
-  resources :names do collection do get "json" end end
-  resources :proper_names
-  resources :uploaded_checks
+  resources :profiles, only: [:index]
+  resources :names, only: [:index] do collection do get "json" end end
+  resources :proper_names, only: [:index]
+  resources :uploaded_checks, only: [:index]
   get "privacy", :to => "top_page#privacy", :as => "privacy"
   root 'top_page#index'
 
