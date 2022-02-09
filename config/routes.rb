3d443be03a5075e0972bdc669157115052d770dc
shiro <shiro.mns@gmail.com>
2022-02-09 16:43:56 +0900

    [変更]collection機能を用いる方法でルーティングを記述する
    ・閲覧時のURLに変更がかかるが、既にツイートなどで告知しているため以前のURLも残しておく

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  resources :obsolescences,   only: [:index] do collection do get "json"; get "pno_text"  end end
-  resources :spells,          only: [:index] do collection do get "json"; get "pno_text"  end end
-  resources :tune_gem_data,   only: [:index] do collection do get "json"; get "pno_text"  end end
+  resources :obsolescences,   only: [:index] do collection do get "json"; get "pno_text" end end
+  resources :spells,          only: [:index] do collection do get "json"; get "pno_text" end end
+  resources :tune_gem_data,   only: [:index] do collection do get "json"; get "pno_text" end end
   resources :spell_data,      only: [:index] do collection do get "json"; get "pno_text" end end
-  resources :items,           only: [:index] do collection do get "json"; get "pno_text"  end end
+  resources :items,           only: [:index] do collection do get "json"; get "pno_text" end end
   get "equip/graphs", :to => "equips#graphs", :as => "equip_graphs"
-  resources :equips,          only: [:index] do collection do get "json"; get "pno_text"  end end
-  resources :statuses,        only: [:index] do collection do get "json"; get "pno_text"  end end
+  resources :equips,          only: [:index] do collection do get "json"; get "pno_text"; get 'graphs' end end
+  resources :statuses,        only: [:index] do collection do get "json"; get "pno_text" end end
   get "profile/graphs", :to => "profiles#graphs", :as => "profile_graphs"
-  resources :profiles,        only: [:index] do collection do get "json"; get "pno_text"  end end
-  resources :names,           only: [:index] do collection do get "json"; get "pno_text"  end end
-  resources :proper_names,    only: [:index] do collection do get "json"; get "pno_text"  end end
-  resources :uploaded_checks, only: [:index] do collection do get "json"; get "pno_text"  end end
+  resources :profiles,        only: [:index] do collection do get "json"; get "pno_text"; get 'graphs' end end
+  resources :names,           only: [:index] do collection do get "json"; get "pno_text" end end
+  resources :proper_names,    only: [:index] do collection do get "json"; get "pno_text" end end
+  resources :uploaded_checks, only: [:index] do collection do get "json"; get "pno_text" end end
   get "privacy", :to => "top_page#privacy", :as => "privacy"
   root 'top_page#index'
 
